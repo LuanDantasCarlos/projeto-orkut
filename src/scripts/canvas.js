655b16ad1d2c48f19a4b6449afe7b3d40f52ef87
Luan Dantas <luandc29@gmail.com>
2022-05-17 15:11:45 -0300

    captura de dados
    realizado a captura dos dados inseridos nos inputs, conversão do texto para imagem

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,19 +2,14 @@ const preview = document.querySelector('#canvas-preview');
 const ctx = preview.getContext('2d');
 const { width, height } = preview;
 
+
 const X = (width/2) - (500/2);
 const Y = (height/2) - (400/2);
 
 
 
-ctx.fillStyle = '#00FF00';
-ctx.fillRect(250, 150, 430, 300);
-
-
-ctx.font = '30px Arial';
-ctx.fillStyle = '#fff';
-ctx.textAlign = 'center';
-ctx.fillText('testando o texto', width/2, height/2);
+//ctx.fillStyle = '#00FF00';
+//ctx.fillRect(250, 150, 430, 300);
 
 
 ctx.fillStyle = '#ff8f';
@@ -23,19 +18,30 @@ ctx.fillRect(3, 190, 140, 40);
 ctx.fillStyle = '#ff5f';
 ctx.fillRect(320, 498, 380, 20);
 
+const btn = document.getElementById("submit");
 
-const btn = document.getElementById('submit');
-
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function(e) {
 
     e.preventDefault();
+    const name = document.querySelector("#name");
+    const comunidade = document.querySelector("#comunidade");
+    const texto = document.querySelector("#texto");
 
-    const name = document.querySelector('#name');
+    const valueName = name.value;
+    const valueComunidade = comunidade.value;
+    const valueTexto = texto.value;
 
-    const value = form.value;
 
-    console.log(value)
-
+    ctx.font = '5 Courier New 50px';
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = 'space-betw';
+    var i in (valueName); {
+        ctx.fillText(valueName[i], 500, 400);
+    }
+    document.getElementById("result")
+    const img = ctx.canvas.toDataURL('#result');
+    
 });
+
 
 
